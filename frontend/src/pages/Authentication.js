39287b1c627a9cc1638f63bac4edb34baed2387d
Anastasiia <anastasiia.shtepa1990@gmail.com>
2024-04-02 21:12:57 +0200

    added logout

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -35,6 +35,9 @@ export async function action({ request }) {
   if (!response.ok) {
     throw json({ message: "Couldn't authenticate user. " }, { status: 500 });
   }
- 
+  const resData = await response.json();
+  const token = resData.token;
+  localStorage.setItem("token", token);
+
   return redirect("/");
 }
